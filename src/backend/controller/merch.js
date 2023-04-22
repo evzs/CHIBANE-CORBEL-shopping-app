7d5fffb06b67631b2d8735e5b9165d5fab5f2a61
RathGate <mariannecorbel@hotmail.fr>
2023-04-22 16:45:19 +0200

    added new routes on API

--- src/backend/controller/merch.js
+++ src/backend/controller/merch.js
@@ -1,8 +1,6 @@
 const old_data = require("../old_data.json");
 const data = require("../data.json")
 exports.getAllItems = (req, res) => {
-    var separated = req.query.display
-    console.log(separated)
     const items = data.items;
     if (!items) {
         res.status(404).json({
@@ -14,51 +12,12 @@ exports.getAllItems = (req, res) => {
         message: "All items found successfully",
         items
     })
-} 
-
-exports.getSneakers = (req, res) => {
-    const sneakers = old_data.sneakers
-    if (!sneakers) {
-        res.status(404).json({
-            message: "Sneakers not found"
-        })
-        return
-    }
-    res.status(200).json({
-        message: "Sneakers found successfully",
-        sneakers
-    })
-}
-
-exports.getSneakerByID = (req, res) => {
-    const id = parseInt(req.params.id)
-    const sneakers = old_data.sneakers
-    const sneaker = sneakers.find(s => s.id === id)
-    if (!sneaker) {
-        res.status(404).json({
-            message: "Sneaker not found"
-        })
-        return
-    }
-    res.status(200).json({
-        message: "Sneaker found successfully",
-        sneaker
-    }) 
 }
 
 exports.getItemByID = (req, res) => {
     const id = parseInt(req.params.id)
-    const items = data.items
-    const item = (function () {
-        for (let i = 0; i < items.length; i++) {
-            let result = items[i].variants.find(i => i.id == id)
-            if (typeof result != "undefined") {
-                return result
-            }
-        }
-        return null
-    })()
-    if (!item) {
+    const result = data.items.find(item => item.id == id)
+    if (!result) {
         res.status(404).json(
             {"title": "An error occurred",
             "status": 404,
@@ -67,11 +26,61 @@ exports.getItemByID = (req, res) => {
             return
     }
     res.status(200).json({
-        item
+        message: "Item found successfully.",
+        items: result
     })
 }
 
 exports.getItemsByCategoryName = (req, res) => {
-    const categoryName = req.params.name 
-    console.log(categoryName.toLowerCase())
+    const categoryName = req.params.name.toLowerCase()
+    const result = data.items.filter(item => item.category.toLowerCase() == categoryName)
+    if (!result) {
+        res.status(404).json(
+            {"title": "An error occurred",
+            "status": 404,
+            "message": "Items not found."}
+        )
+            return
+    }
+    res.status(200).json({
+    message: "All items found successfully",
+    items: result
+    })
+}
+exports.getItemsBySubName = (req, res) => {
+    const categoryName = req.params.name.toLowerCase()
+    const subcategoryName = req.params.subname.toLowerCase()
+    const result = data.items.filter(item => {
+        return item.category.toLowerCase() == categoryName && item.subcategory.toLowerCase() == subcategoryName
+    })
+    if (!result) {
+        res.status(404).json(
+            {"title": "An error occurred",
+            "status": 404,
+            "message": "Items not found."}
+        )
+            return
+    }
+    res.status(200).json({
+    message: "All items found successfully",
+    items: result
+    })
+}
+
+exports.getAllData = (req, res) => {
+    console.log(data)
+    if (!data) {
+        res.status(404).json(
+            {
+                "title": "An error occurred",
+                "status": 404,
+                "message": "Items not found."
+            }
+        )
+        return
+    }
+    res.status(200).json({
+        message: "All data found successfully",
+        data
+    })
 }
--- src/backend/routes/merch.js
+++ src/backend/routes/merch.js
@@ -2,11 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const controller = require("../controller/merch")
-
-router.get("/sneaker/:id", controller.getSneakerByID)
-router.get("/sneakers/", controller.getSneakers)
+router.get("/", controller.getAllData)
 router.get("/items", controller.getAllItems)
 router.get("/items/:id", controller.getItemByID)
 router.get("/items/category/:name", controller.getItemsByCategoryName)
+router.get("/items/category/:name/:subname", controller.getItemsBySubName)
 
 module.exports = router
