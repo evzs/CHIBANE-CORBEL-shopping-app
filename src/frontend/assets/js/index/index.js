a65211f6fe5df773ae6af0bf314af9139473f24b
RathGate <mariannecorbel@hotmail.fr>
2023-05-11 14:02:12 +0200

    added sort functionality

--- src/frontend/assets/js/index/index.js
+++ src/frontend/assets/js/index/index.js
@@ -1,9 +1,9 @@
 const url = "http://localhost:5501/"
-let data;
-
+let currentCategory = -1;
+let currentSubCategory = -1;
 // *---- CATEGORIES --- //
 
-function loadCategories() {
+function getCategories() {
     fetch(url + "categories/")
     .then(response => response.json())
     .then(response => {
@@ -17,21 +17,20 @@ function loadCategories() {
 } 
 
 function selectCategory(catTitle, subcatTitle = null) {
-
     document.querySelectorAll(".cat-title,.subcat-title").forEach(div => {
         div.classList.remove("selected")
     })
     catTitle.classList.add("selected")
-    
     if (subcatTitle) {
         subcatTitle.classList.add("selected")
     }
+    unsetFilters()
 }
 
 function changeTitle(category, subcategory = "") {
     let titleDiv = document.querySelector("h1")
     let subtitleDiv = document.querySelector(".sub")
-    
+
     if (titleDiv) {
         titleDiv.innerHTML = subcategory ? subcategory : `All ${category}`;
     }
@@ -43,6 +42,8 @@ function changeTitle(category, subcategory = "") {
 function generateCategories(categories) {
     // Adds listener to the "Show all" link:
     document.querySelector(".show-all").addEventListener("click", function () {
+        currentCategory = -1
+        currentSubCategory = -1
         selectCategory(this)
         changeTitle("Articles")
         updateItems()
@@ -57,6 +58,8 @@ function generateCategories(categories) {
         let catCtn = document.createElement("div"); catCtn.classList.add("category-ctn")
         let catTitle = document.createElement("div"); catTitle.classList.add("cat-title"); catTitle.innerHTML = `${category.name}`
         catTitle.addEventListener("click", function () {
+            currentCategory = category.id
+            currentSubCategory = -1
             selectCategory(catTitle)
             changeTitle(category.name)
             updateItems(category.id)
@@ -78,6 +81,8 @@ function generateSubCategories(category, catContainer) {
     category.subcategories.forEach(subcategory => {
         let subcatTitle = document.createElement("div"); subcatTitle.classList.add("subcat-title"); subcatTitle.innerHTML = `${subcategory.name}`
         subcatTitle.addEventListener("click", function () {
+            currentCategory = category.id
+            currentSubCategory = subcategory.id
             selectCategory(catContainer, subcatTitle)
             changeTitle(category.name, subcategory.name)
             updateItems(category.id, subcategory.id)
@@ -87,20 +92,25 @@ function generateSubCategories(category, catContainer) {
     return subcatContainer
 }
 
-loadCategories()
+getCategories()
 
 // *------------------- //
 
 // *----- ARTICLES ---- //
 
-function loadArticles(endpoint = "") {
+function getArticles(endpoint = "", filters = {}) {
     fetch(url + `items/${endpoint}`)
     .then(response => response.json())
-        .then(response => {
+    .then(response => {
+        // VIDE?
         if (!response || !response.items) {
             return
-        }
-        generateArticles(response.items)
+        } else if (!Object.keys(filters).length) {
+            generateArticles(response.items)
+        } else {
+            filteredItems = applyFilters(response.items, filters)
+            generateArticles(filteredItems)
+        } 
         return response.items
     })
     .catch(error => console.log(`Error while fetching url: ${error}`))
@@ -112,10 +122,15 @@ function generateArticles(items) {
         return
     }
     container.innerHTML = "";
-    items.forEach(item => generateBaseItem(item, container))
+    if (items) {
+        items.forEach(item => generateBaseItem(item, container))
+    } else {
+        console.log("no items to show ?")
+    }
+    
 }
 
-function generateBaseItem(item, containerDiv) {
+function generateBaseItem(item, container) {
     let itemDiv = document.createElement("div")
     itemDiv.classList.add("article-item")
     itemDiv.innerHTML = `
@@ -125,23 +140,23 @@ function generateBaseItem(item, containerDiv) {
         ${item.reduction ? '<div class="reduction">-'+item.reduction+'%</div>' : ''}
     </div>
     <div class="article-info">
-        <div class="brand">${item.cat}</div>
+        <div class="brand">${item.brand}</div>
         <div class="name">${item.title}</div>
         <div class="price-see-more">
-            ${generatePrice(item.price, item.reduction)}
+            ${generatePrice(item)}
             <a href="article.html?articleID=${item.id}" target="_blank" class="see-more">See more >></a>
         </div>
     </div>`
-    containerDiv.appendChild(itemDiv)
+    container.appendChild(itemDiv)
 }
 
-function generatePrice(basePrice, reduction) {
-    return reduction ?
-        `<div class="price"><span class="new">${addZeros(basePrice - (basePrice * reduction /100))}€</span><span class="former">${addZeros(basePrice)}€</span></div>`
-        : `<div class="price">${addZeros(basePrice)}€</div>`
+function generatePrice(item) {
+    return item.reduction ?
+        `<div class="price"><span class="new">${addZeros(item.price - (item.price * item.reduction /100))}€</span><span class="former">${addZeros(item.price)}€</span></div>`
+        : `<div class="price">${addZeros(item.price)}€</div>`
 
 } 
-loadArticles()
+getArticles()
 
 function addZeros(number) {
     number = Number(number)
@@ -151,15 +166,26 @@ function addZeros(number) {
     return number.toLocaleString("en",{useGrouping: false,minimumFractionDigits: 2});
 }
 
-function updateItems(catID=-1, subcatID=-1) {
+function updateItems(catID = -1, subcatID = -1, filters = []) {
     if (catID < 0) {
-        loadArticles()
+        getArticles("", filters)
     } else if (subcatID < 0) {
-        loadArticles(`category/${catID}`)
+        getArticles(`category/${catID}`, filters)
     } else {
-       loadArticles(`category/${catID}/${subcatID}`)
+        getArticles(`category/${catID}/${subcatID}`, filters)
     }
 } 
+
+function removeClassFromAll(divArr, className) {
+    if (divArr.length == 0) {
+        return
+    }
+    divArr.forEach(div => {
+        div.classList.remove(className)
+    })
+}
+
+const ALL_FILTERS = ["color", "brand", "size"];
 
 // ?SEARCH PAGE - Filters
 let filters = document.querySelectorAll(".filter-container")
@@ -202,7 +228,6 @@ function countAllChecked(checkboxes) {
     return total
 }
 
-
 document.addEventListener("click", function(e) {
     let filters = document.querySelectorAll(".filter-options-container")
     let $target = $(e.target)
@@ -213,15 +238,72 @@ document.addEventListener("click", function(e) {
     || $target.hasClass("filter-title activable")){
         return
     }
-    
     removeClassFromAll(filters, "visible")
 })
 
-function removeClassFromAll(divArr, className) {
-    if (divArr.length == 0) {
-        return
-    }
-    divArr.forEach(div => {
-        div.classList.remove(className)
+
+
+let form = document.getElementById("filters-form")
+let filtersInputs = form.querySelectorAll("input[type='checkbox'], input[type='radio']")
+
+if (form) {
+    form.addEventListener("submit", function (e) {
+        e.preventDefault()
     })
+}
+
+if (filtersInputs) {
+    filtersInputs.forEach(input => {
+        input.addEventListener("change", function() {
+            updateItems(currentCategory, currentSubCategory, retrieveFilters())
+        })
+    })
+}
+function unsetFilters() {
+    document.querySelectorAll("input[type='checkbox']").forEach(input => input.checked = false)
+    document.querySelectorAll(".count .number").forEach(element => element.innerHTML = "all")
+}
+function applyFilters(items = [], filters = {}) {
+    if (!Object.keys(filters) || !items) {
+        return items
+    }
+    switch (filters["sort-by"]) {
+        case "price-low":
+            items.sort((a, b) => {
+                return (a.price - (a.price * a.reduction /100)) - (b.price - (b.price * b.reduction /100))
+            })
+            break;
+        case "price-high":
+            items.sort((a, b) => {
+                return (b.price - (b.price * b.reduction / 100)) - (a.price - (a.price * a.reduction / 100))
+            })
+            break;
+        case "sale":
+            items.sort((a, b) => {
+                if (b - a == 0) {
+                    return (b.price - (b.price * b.reduction / 100)) - (a.price - (a.price * a.reduction / 100))
+                }
+                return (b.reduction - a.reduction)
+            })
+            break;
+        case "default":
+            items.sort((a, b) => {
+                return (b.id - a.id)
+            })
+            break;
+    }
+    return items
+}
+
+
+function retrieveFilters() {
+    let filters = { "filter-list": [], "sort-by": [] }
+    let sortValue = document.querySelector("input[name='sort-by']:checked").value;
+    
+    filters["sort-by"] = sortValue ? sortValue : "no-sort";
+    filters["filter-list"] = ALL_FILTERS.map(filter => {
+        temp = {};
+        return temp[filter] = Array.from(document.querySelectorAll(`input[name='${filter}']:checked`), e => e.value)
+    })
+    return filters
 }
